60e15fd07354038de36055ed6ceab9de1f1e769b
Gustav Lindqvist <hej@gustavlindqvist.se>
2023-03-07 20:49:43 +0100

    remove comment

--- src/_data/goodreads.js
+++ src/_data/goodreads.js
@@ -52,8 +52,6 @@ module.exports = async () => {
         }
     };
 
-    console.log(await getPosts('currently-reading',feed_id));
-
     return {
         read: await getPosts('read',feed_id),
         currently_reading: await getPosts('currently-reading',feed_id),
