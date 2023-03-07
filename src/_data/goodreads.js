953ff7db7e6246531d85bc5c218564e906c4215b
Gustav Lindqvist <gustav.lindqvist@ju.se>
2023-03-07 16:15:56 +0100

    add more fields to goodreads data

--- src/_data/goodreads.js
+++ src/_data/goodreads.js
@@ -5,7 +5,17 @@ module.exports = async () => {
     let parser = new Parser({
         timeout: 10000,
         customFields: {
-            item: ['book_id', 'book', 'author_name'],
+            item: [
+                'book_id',
+                'book',
+                'book_description',
+                'author_name',
+                'book_large_image_url',
+                'book_small_image_url',
+                'user_name',
+                'user_date_added',
+                'user_read_at',
+            ],
         }
     });
 
@@ -28,8 +38,13 @@ module.exports = async () => {
             feed.items.forEach((item) => {
                 item.date = new Date(item.pubDate);
                 item.url = 'https://www.goodreads.com/book/show/' + item.book_id;
-                item.pages = item.book.num_pages[0] + ' sidor';
+                item.pages = item.book.num_pages[0];
                 item.author = item.author_name;
+                item.user = {
+                    name: item.user_name,
+                    date_added: new Date(item.user_date_added),
+                    date_read_at: new Date(item.user_read_at),
+                }
             });
             return feed.items;
         } catch (err) {
