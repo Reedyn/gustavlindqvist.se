c93fa8ecae4a8f1497caeb6e4843e6a35d28af37
Gustav Lindqvist <hej@gustavlindqvist.se>
2023-03-18 19:46:22 +0100

    reduce size of images for linktips

--- src/_data/inoreader.js
+++ src/_data/inoreader.js
@@ -187,7 +187,7 @@ module.exports = async () => {
                         .filter((tag) => tag !== 'Good shit')
                         .map((tag) => tag.toLowerCase())
                     : [];
-                outputPost.feature_image = `https://opengraph.gustavlindqvist.se/${encodeURIComponent(post.link)}`;
+                outputPost.feature_image = `https://opengraph.gustavlindqvist.se/${encodeURIComponent(post.link)}/small/webp/`;
                 outputPosts.push(outputPost);
             });
 
