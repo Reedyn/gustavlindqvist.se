a58d9a31893bb3c17c655c2c54eaaebbcaebecc8
Gustav Lindqvist <gustav.lindqvist@ju.se>
2023-03-31 08:16:08 +0200

    move scripts into folders

---
+++ src/assets/javascript/404.js
@@ -0,0 +1,4 @@
+[...document.querySelectorAll('.url-not-found')].forEach((element) => {
+    element.href = element.getAttribute('data-url-prefix') + window.location.href;
+});
+document.getElementById('extra-url-info').classList.remove('hidden');
---
+++ src/assets/javascript/comments.js
@@ -0,0 +1,28 @@
+const commentForm = document.getElementById('comment-form');
+
+document.getElementById('content').addEventListener('focus', (event) => {
+    commentForm.classList.remove('-inactive');
+    event.target.attributes.placeholder.value = '';
+});
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let formStatus = document.getElementById('form-status');
+    let formData = new FormData(commentForm);
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: new URLSearchParams(formData).toString()
+    })
+        .then(() => {
+            commentForm.classList.add('hidden');
+            formStatus.innerText = 'Tack för din kommentar! Hanteringen sker manuellt och publiceringen sker vanligtvis inom någon dag.';
+            formStatus.classList.remove('hidden');
+
+        })
+        .catch((error) => alert(error));
+};
+
+document
+    .querySelector('form')
+    .addEventListener('submit', handleSubmit);
