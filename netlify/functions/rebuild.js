96430014c7d0054ee715abfddb4f9a4be2f8a827
Gustav Lindqvist <gustav.lindqvist@ju.se>
2024-12-09 08:11:42 +0100

    rewrote schedule build function

--- netlify/functions/rebuild.js
+++ netlify/functions/rebuild.js
@@ -1,32 +1,14 @@
-export async function handler(event, context) {
-	try {
-		// eslint-disable-next-line no-undef
-		const response = await fetch(Netlify.env.get('BUILD_HOOK'), {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
+export default async (req) => {
+	const { next_run } = await req.json();
 
-		const data = await response.json().catch(() => null);
+	// eslint-disable-next-line no-undef
+	const response = await fetch(Netlify.env.get('BUILD_HOOK'), {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-
-		return {
-			statusCode: 200,
-			body: JSON.stringify({
-				message: 'POST request successful',
-				response: data,
-			}),
-		};
-	} catch (error) {
-		return {
-			statusCode: 500,
-			body: JSON.stringify({
-				error: error.message,
-			}),
-		};
-	}
-}
+	// eslint-disable-next-line no-console
+	console.log('Posted build. Next invocation at:', next_run);
+};
