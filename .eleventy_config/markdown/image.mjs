565f8d5e3fcbdf73e0e5ebe58dc80c94eabec7cf
Gustav Lindqvist <hej@gustavlindqvist.se>
2025-01-01 13:46:10 +0100

    add support for imgproxy

--- .eleventy_config/markdown/image.mjs
+++ .eleventy_config/markdown/image.mjs
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import EleventyImage from '@11ty/eleventy-img';
 import markdownIt from 'markdown-it';
+import path from 'node:path';
+import ImgProxy from 'imgproxy';
 
 let markdown = markdownIt({
 	html: true,
@@ -14,9 +16,12 @@ export default function (tokens, index, options, env) {
 	const token = tokens[index];
 	const source = decodeURI(token.attrGet('src'));
 
-	token.tag = 'figure';
-	token.type = 'block';
-
+	const imgProxy = new ImgProxy({
+		baseUrl: process.env.IMGPROXY_HOST,
+		key: process.env.IMGPROXY_KEY,
+		salt: process.env.IMGPROXY_SALT,
+		encode: true,
+	});
 	const attributes = {
 		alt: token.content,
 		loading: 'lazy',
@@ -24,105 +29,142 @@ export default function (tokens, index, options, env) {
 	};
 	const style = token.attrGet('class') ? token.attrGet('class') : '';
 	const classString = ` class="image ${style}"`;
-
 	const caption = token.attrGet('title');
-
 	const attributesString =
 		' ' +
 		Object.keys(attributes)
 			.map((key) => `${key}="${attributes[key]}"`)
 			.join(' ');
 
+	// Where is the source file located?
+	let sourceLocation;
 	if (source === '') {
-		if (caption) {
-			return `<figure${classString}><img width="800" height="300"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
-		}
-		return `<figure${classString}><img width="800" height="300"${attributesString}></figure>`;
-	} else if (source.startsWith('/') || source.startsWith('http')) {
-		// If the source is absolute or external
-		if (caption) {
-			return `<figure${classString}><img src="${source}"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
-		}
-		return `<img src="${source}"${classString}${attributesString}>`;
+		sourceLocation = 'null';
+	} else if (source.startsWith('/')) {
+		sourceLocation = 'relative-to-root';
+	} else if (source.startsWith('http')) {
+		sourceLocation = 'external';
 	} else if (!env.page.outputPath) {
-		return `<figure${classString}><img width="800" height="300"${attributesString}></figure>`;
+		sourceLocation = 'null';
 	} else {
-		// If relative, local image
-		const documentPath = env.page.filePathStem;
-		const outputPath = env.page.outputPath
-			.substring(0, env.page.outputPath.lastIndexOf('/')) // Remove document from path
-			.replace(/^\//, ''); // remove first slash
+		sourceLocation = 'local';
+	}
 
-		// If the image is absolute path or external
-		const folderPath =
-			'./src/' +
-			documentPath
-				.substring(0, documentPath.lastIndexOf('/') + 1) // Remove document from path
+	switch (sourceLocation) {
+		case 'null':
+			if (caption) {
+				return `<figure${classString}><img width="800" height="300"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
+			}
+			return `<figure${classString}><img width="800" height="300"${attributesString}></figure>`;
+
+		case 'relative-to-root':
+			if (caption) {
+				return `<figure${classString}><img src="${source}"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
+			}
+			return `<img src="${source}"${classString}${attributesString}>`;
+
+		case 'external':
+			if (caption) {
+				return `<figure${classString}><img src="${source}"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
+			}
+			return `<img src="${source}"${classString}${attributesString}>`;
+
+		case 'local': {
+			const documentPath = env.page.filePathStem;
+			const outputPath = env.page.outputPath
+				.substring(0, env.page.outputPath.lastIndexOf('/')) // Remove document from path
 				.replace(/^\//, ''); // remove first slash
 
-		const options = {
-			widths: [480, 800, 1080, 1620, 2430, null],
-			formats: [null],
-			outputDir: outputPath,
-			urlPath: '',
-			sharpGifOptions: {
-				animated: true,
-			},
-			sharpJpegOptions: {
-				progressive: true,
-				optimiseScans: true,
-			},
-		};
+			// If the image is absolute path or external
+			const folderPath =
+				'./src/' +
+				documentPath
+					.substring(0, documentPath.lastIndexOf('/') + 1) // Remove document from path
+					.replace(/^\//, ''); // remove first slash
 
-		const filePath = folderPath + source;
+			const widths = [480, 800, 1080, 1620, 2430];
+			const options = {
+				widths: [null],
+				formats: [null],
+				outputDir: outputPath,
+				urlPath: '',
+				filenameFormat: function (id, src, width, format) {
+					const extension = path.extname(src);
+					const name = path.basename(src, extension);
 
-		EleventyImage(filePath, options);
-		const metadata = EleventyImage.statsSync(filePath, options);
-		console.log(
-			'[' + '\x1b[36m%s\x1b[0m',
-			'11ty Image' + '\x1b[0m' + ']:',
-			'Created responsive images for',
-			filePath,
-		);
+					return `${name}.${format}`;
+				},
+			};
 
-		let format = '';
-		for (const key in metadata) {
-			format = key;
-		}
+			const filePath = folderPath + source;
 
-		let lowsrc = metadata[format].length > 1 ? metadata[format][1] : metadata[format][0];
-		let highsrc = metadata[format][metadata[format].length - 1];
+			EleventyImage(filePath, options);
+			const metadata = EleventyImage.statsSync(filePath, options);
+			console.log(
+				'[' + '\x1b[36m%s\x1b[0m',
+				'11ty Image' + '\x1b[0m' + ']:',
+				'Create image element for ',
+				filePath,
+			);
 
-		let inlineStyling =
-			style === '-inline' ? ` style="flex: ${highsrc.width / highsrc.height}"` : '';
+			let format = '';
+			for (const key in metadata) {
+				format = key;
+			}
 
-		// Base sizes on the layout changes.
-		let sizes = '(max-width: 50rem) 100vw, 50rem';
-		switch (style) {
-			case '-full':
-				sizes = '100vw';
-				break;
-			case '-wide':
-				sizes = '(max-width: 80rem) 100vw, 80rem';
-				break;
-			case '-inline':
-				// Approximation of the size in the UI, not perfect since the siblings width isn't taken into account
-				sizes = `(max-width: 40em) 100vw, (max-width: 65em) ${(highsrc.width / highsrc.height) * 40}vw, ${(highsrc.width / highsrc.height) * 40}rem`;
-				break;
-		}
+			let imageSrc = metadata[format][0];
 
-		const captionElement = caption
-			? `<figcaption>${markdown.render(caption)}</figcaption>`
-			: '';
+			let inlineStyling =
+				style === '-inline' ? ` style="flex: ${imageSrc.width / imageSrc.height}"` : '';
 
-		return `<figure class="image ${style}"${inlineStyling}><picture>${Object.values(metadata)
-			.map((imageFormat) => {
-				return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map((entry) => entry.srcset).join(', ')}" sizes="${sizes}">`;
-			})
-			.join('\n')}<img
-            src="${lowsrc.url}"
-            width="${lowsrc.width}"
-            height="${lowsrc.height}"
+			// Base sizes on the layout changes.
+			let sizes = '(max-width: 50rem) 100vw, 50rem';
+			switch (style) {
+				case '-full':
+					sizes = '100vw';
+					break;
+				case '-wide':
+					sizes = '(max-width: 80rem) 100vw, 80rem';
+					break;
+				case '-inline':
+					// Approximation of the size in the UI, not perfect since the siblings width isn't taken into account
+					sizes = `(max-width: 40em) 100vw, (max-width: 65em) ${(imageSrc.width / imageSrc.height) * 40}vw, ${(imageSrc.width / imageSrc.height) * 40}rem`;
+					break;
+			}
+
+			const captionElement = caption
+				? `<figcaption>${markdown.render(caption)}</figcaption>`
+				: '';
+			console.log(
+				imgProxy
+					.builder()
+					.width(300)
+					.generateUrl(
+						'https://gustavlindqvist.se/2024/10/09/skolgardskarta-till-talavidskolan/3Rx9ljY8jB-1589.jpeg',
+					),
+			);
+
+			const srcset = widths
+				.map((width) => {
+					return (
+						imgProxy.builder().width(width).generateUrl(imageSrc.url) +
+						' ' +
+						width +
+						'w'
+					);
+				})
+				.join(', ');
+
+			const sourceElement = !process.env.HOST.includes('localhost')
+				? `<source type="${imageSrc.sourceType}" srcset="${srcset}" sizes="${sizes}">`
+				: '';
+
+			return `<figure class="image ${style}"${inlineStyling}><picture>${sourceElement}
+			<img
+            src="${imageSrc.url}"
+            width="${imageSrc.width}"
+            height="${imageSrc.height}"
             ${attributesString}></picture>${captionElement}</figure>`;
+		}
 	}
 }
