8a4b9f0e4024db7010c57c43d8272201dc7b20a0
Gustav Lindqvist <gustav.lindqvist@ju.se>
2025-11-27 12:34:21 +0100

    minor cleanup in image shortcode

--- .eleventy_config/shortcodes/image.mjs
+++ .eleventy_config/shortcodes/image.mjs
@@ -45,12 +45,6 @@ export default function (src, style, alt, sizes, caption = undefined) {
 			}
 			return `<figure${classString}><img width="800" height="300"${attributesString}></figure>`;
 
-		case 'relative-to-root':
-			if (caption) {
-				return `<figure${classString}><img src="${src}"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
-			}
-			return `<img src="${src}"${classString}${attributesString}>`;
-
 		case 'external':
 			if (caption) {
 				return `<figure${classString}><img src="${src}"${attributesString}><figcaption><em>${markdown.render(caption)}</em></figcaption></figure>`;
@@ -70,7 +64,7 @@ export default function (src, style, alt, sizes, caption = undefined) {
 			const metadata = EleventyImage.statsSync(filePath);
 			console.log(
 				'[' + '\x1b[36m%s\x1b[0m',
-				'Markdown Image' + '\x1b[0m' + ']:',
+				'Image Shortcode' + '\x1b[0m' + ']:',
 				'Create image element for ',
 				filePath,
 			);
