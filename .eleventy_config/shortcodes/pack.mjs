d491c99d7c516941a9fe911785ebf37e6b4b7019
Gustav Lindqvist <gustav@gustavlindqvist.se>
2026-03-15 17:22:03 +0100

    change classes for pack and statistics

--- .eleventy_config/shortcodes/pack.mjs
+++ .eleventy_config/shortcodes/pack.mjs
@@ -11,50 +11,50 @@ export default {
 					typeof category.color !== 'undefined' && category.color.length
 						? category.color
 						: 'oklch(60% 0 0)';
-				outputString += `<details class="pack__category"><summary class="pack__category-summary">
-                    <span class="pack__category-summary__left"><svg class="icon -large" role="presentation" style="color: ${color}" aria-hidden="true" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/${category.icon}.svg#icon"></use></svg></span><span class="pack__category-summary__middle">${category.name}<span class="sr-only">:</span></span>
-                    <span class="pack__category-summary__right" style="padding-right: 0.3rem">${prettyDigits(category.total_weight)}g</span>
-                    <div class="pack__category-summary__bar" style="width: ${((category.total_weight / pack.total_weight) * 200).toFixed(3)}%; background: ${color};"></div></summary>`;
+				outputString += `<details class="category"><summary class="category-summary">
+                    <span class="category-summary__left"><svg class="icon -large" role="presentation" style="color: ${color}" aria-hidden="true" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/${category.icon}.svg#icon"></use></svg></span><span class="category-summary__middle">${category.name}<span class="sr-only">:</span></span>
+                    <span class="category-summary__right" style="padding-right: 0.3rem">${prettyDigits(category.total_weight)}g</span>
+                    <div class="category-summary__bar" style="width: ${((category.total_weight / pack.total_weight) * 200).toFixed(3)}%; background: ${color};"></div></summary>`;
 				let packList = '';
 
-				packList += '<ul class="pack__list">';
+				packList += '<ul class="list">';
 				category.items.forEach((item) => {
 					const quantity = typeof item.quantity !== 'undefined' ? item.quantity : 1;
 					let metaString = '';
 					if (typeof item.worn !== 'undefined' && item.worn) {
 						metaString =
-							' <span class="pack__list-item__bottom-right secondary"><svg class="icon" role="presentation" style="color: #5E35B1" aria-label="Buren" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/tshirt-crew.svg#icon"></use></svg></span>';
+							' <span class="list-item__bottom-right secondary"><svg class="icon" role="presentation" style="color: #5E35B1" aria-label="Buren" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/tshirt-crew.svg#icon"></use></svg></span>';
 					} else if (typeof item.consumable !== 'undefined' && item.consumable) {
 						metaString =
-							' <span class="pack__list-item__bottom-right secondary">Förbrukningsvara</span>';
+							' <span class="list-item__bottom-right secondary">Förbrukningsvara</span>';
 					}
 					if (typeof item.item.brand !== 'undefined' && item.item.brand !== null) {
-						packList += `<li class="pack__list-item"><span class="pack__list-item__left secondary">${quantity}st</span> <span class="pack__list-item__bottom secondary">${item.item.name}<span class="sr-only">.</span></span> <span class="pack__list-item__middle"><span class="light">${typeof item.item.brand !== 'undefined' && item.item.brand !== null ? item.item.brand.name : ''}</span> <span class="bold">${typeof item.item.product !== 'undefined' && item.item.product !== null ? item.item.product.name : ''}${typeof item.item.product_variant !== 'undefined' && item.item.product_variant !== null ? ' ' + item.item.product_variant.name : ''}</span><span class="sr-only">.</span></span> <span class="pack__list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
+						packList += `<li class="list-item"><span class="list-item__left secondary">${quantity}st</span> <span class="list-item__bottom secondary">${item.item.name}<span class="sr-only">.</span></span> <span class="list-item__middle"><span class="light">${typeof item.item.brand !== 'undefined' && item.item.brand !== null ? item.item.brand.name : ''}</span> <span class="bold">${typeof item.item.product !== 'undefined' && item.item.product !== null ? item.item.product.name : ''}${typeof item.item.product_variant !== 'undefined' && item.item.product_variant !== null ? ' ' + item.item.product_variant.name : ''}</span><span class="sr-only">.</span></span> <span class="list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
 					} else {
-						packList += `<li class="pack__list-item"><span class="pack__list-item__left secondary">${quantity}st</span> <span class="pack__list-item__middle bold">${item.item.name}<span class="sr-only">.</span></span> <span class="pack__list-item__bottom secondary">${item.item.notes ? item.item.notes : ''}<span class="sr-only">.</span></span> <span class="pack__list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
+						packList += `<li class="list-item"><span class="list-item__left secondary">${quantity}st</span> <span class="list-item__middle bold">${item.item.name}<span class="sr-only">.</span></span> <span class="list-item__bottom secondary">${item.item.notes ? item.item.notes : ''}<span class="sr-only">.</span></span> <span class="list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
 					}
 				});
 				packList += '</ul>';
 				outputString += packList;
 				outputString += '</details>';
 			});
-			outputString += `<ul class="statistics__list ${pack.consumables_weight > 0 ? '-column-count-4' : ''}">
-                    <li class="statistics__list-item">
-                        <span class="statistics__list-item__label">Total vikt<span class="sr-only">:</span></span>
-                        <span class="statistics__list-item__value">${prettyDigits(pack.total_weight)}g</span>
+			outputString += `<ul class="statistics-list ${pack.consumables_weight > 0 ? '-column-count-4' : ''}">
+                    <li class="statistics-list item">
+                        <span class="statistics-list item-label">Total vikt<span class="sr-only">:</span></span>
+                        <span class="statistics-list item-value">${prettyDigits(pack.total_weight)}g</span>
                     </li>
-                    <li class="statistics__list-item">
-                        <span class="statistics__list-item__label">Basvikt<span class="sr-only">:</span></span>
-                        <span class="statistics__list-item__value">${prettyDigits(pack.base_weight)}g</span>
+                    <li class="statistics-list-item">
+                        <span class="statistics-list item-label">Basvikt<span class="sr-only">:</span></span>
+                        <span class="statistics-list item-value">${prettyDigits(pack.base_weight)}g</span>
                     </li>
-                    <li class="statistics__list-item">
-                        <span class="statistics__list-item__label">På kroppen<span class="sr-only">:</span></span>
-                        <span class="statistics__list-item__value">${prettyDigits(pack.worn_weight)}g</span>
+                    <li class="statistics-list-item">
+                        <span class="statistics-list item-label">På kroppen<span class="sr-only">:</span></span>
+                        <span class="statistics-list item-value">${prettyDigits(pack.worn_weight)}g</span>
                     </li>`;
 			if (pack.consumables_weight > 0) {
-				outputString += `<li class="statistics__list-item">
-                            <span class="statistics__list-item__label">Förbrukningsvaror<span class="sr-only">:</span></span>
-                            <span class="statistics__list-item__value">${prettyDigits(pack.consumables_weight)}g</span>
+				outputString += `<li class="statistics-list-item">
+                            <span class="statistics-list item-label">Förbrukningsvaror<span class="sr-only">:</span></span>
+                            <span class="statistics-list item-value">${prettyDigits(pack.consumables_weight)}g</span>
                         </li>`;
 			}
 			outputString += '</ul>';
@@ -71,31 +71,31 @@ export default {
 		if (pack) {
 			let outputString = '<section class="pack">';
 			let equipmentString = '';
-			equipmentString += '<div class="pack__list-container equipment -collapsed">';
+			equipmentString += '<div class="list-container equipment -collapsed">';
 			pack.contents.forEach((item_category) => {
 				let packList = '';
 				const color =
 					typeof item_category.color !== 'undefined' && item_category.color.length
 						? item_category.color
 						: 'var(--color__text)';
-				packList += '<ul class="pack__list">';
+				packList += '<ul class="list">';
 				item_category.items.forEach((item) => {
 					const quantity = typeof item.quantity !== 'undefined' ? item.quantity : 1;
 					let metaString = '';
 					if (typeof item.worn !== 'undefined' && item.worn) {
 						metaString =
-							' <span class="pack__list-item__bottom-right secondary"><svg class="icon" role="presentation" style="color: #5E35B1" aria-label="Buren" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/tshirt-crew.svg#icon"></use></svg></span>';
+							' <span class="list-item__bottom-right secondary"><svg class="icon" role="presentation" style="color: #5E35B1" aria-label="Buren" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/tshirt-crew.svg#icon"></use></svg></span>';
 					} else if (typeof item.consumable !== 'undefined' && item.consumable) {
 						metaString =
-							' <span class="pack__list-item__bottom-right secondary">Förbrukningsvara</span>';
+							' <span class="list-item__bottom-right secondary">Förbrukningsvara</span>';
 					}
 					if (typeof item.item.brand !== 'undefined' && item.item.brand !== null) {
-						packList += `<li class="pack__list-item"><span class="pack__list-item__left secondary">${quantity}st</span> <span class="pack__list-item__bottom secondary">${item.item.name}<span class="sr-only">.</span></span> <span class="pack__list-item__middle"><span class="light">${typeof item.item.brand !== 'undefined' && item.item.brand !== null ? item.item.brand.name : ''}</span> <span class="bold">${typeof item.item.product !== 'undefined' && item.item.product !== null ? item.item.product.name : ''}${typeof item.item.product_variant !== 'undefined' && item.item.product_variant !== null ? ' ' + item.item.product_variant.name : ''}</span><span class="sr-only">.</span></span> <span class="pack__list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
+						packList += `<li class="list-item"><span class="list-item__left secondary">${quantity}st</span> <span class="list-item__bottom secondary">${item.item.name}<span class="sr-only">.</span></span> <span class="list-item__middle"><span class="light">${typeof item.item.brand !== 'undefined' && item.item.brand !== null ? item.item.brand.name : ''}</span> <span class="bold">${typeof item.item.product !== 'undefined' && item.item.product !== null ? item.item.product.name : ''}${typeof item.item.product_variant !== 'undefined' && item.item.product_variant !== null ? ' ' + item.item.product_variant.name : ''}</span><span class="sr-only">.</span></span> <span class="list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
 					} else {
-						packList += `<li class="pack__list-item"><span class="pack__list-item__left secondary">${quantity}st</span> <span class="pack__list-item__middle bold">${item.item.name}<span class="sr-only">.</span></span> <span class="pack__list-item__bottom secondary">${item.item.notes ? item.item.notes : ''}<span class="sr-only">.</span></span> <span class="pack__list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
+						packList += `<li class="list-item"><span class="list-item__left secondary">${quantity}st</span> <span class="list-item__middle bold">${item.item.name}<span class="sr-only">.</span></span> <span class="list-item__bottom secondary">${item.item.notes ? item.item.notes : ''}<span class="sr-only">.</span></span> <span class="list-item__right">${prettyDigits(item.item.weight * quantity)}g</span>${metaString}</li>`;
 					}
 				});
-				equipmentString += `<span class="pack__label">
+				equipmentString += `<span class="label">
                                         <svg class="icon -large" role="presentation" style="color: ${color}" aria-hidden="true" width="12" height="12" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" href="/assets/icons/${item_category.icon}.svg#icon"></use></svg>
                                         <span class="uppercase semibold">${item_category.name}</span>
                                         <span class="secondary">(${prettyDigits(item_category.total_weight)}g)</span>
@@ -104,7 +104,7 @@ export default {
 				equipmentString += '</ul>';
 			});
 			equipmentString +=
-				'<button class="button pack__list-button pack__list-button__show-inventory hidden@no-js">Visa all utrustning</button></div>';
+				'<button class="button list-button list-button__show-inventory hidden@no-js">Visa all utrustning</button></div>';
 			outputString += equipmentString;
 			outputString += '</section>';
 			return outputString;
