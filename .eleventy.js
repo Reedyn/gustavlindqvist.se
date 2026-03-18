9460e0e7c053ddfe3a39d9bb666b985eb0871d88
Gustav Lindqvist <gustav@gustavlindqvist.se>
2026-03-18 19:22:46 +0100

    update url for xsl stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -52,7 +52,6 @@ export default function (eleventyConfig) {
 	});
 
 	eleventyConfig.addPassthroughCopy('src/feed.xsl');
-	eleventyConfig.addPassthroughCopy('src/CNAME');
 	eleventyConfig.addPassthroughCopy('src/robots.txt');
 
 	// Layouts
