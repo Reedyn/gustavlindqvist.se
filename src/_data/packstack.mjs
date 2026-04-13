38d0f798b532bc3401a8af4b3dc1f71d8c12c943
Gustav Lindqvist <gustav@gustavlindqvist.se>
2026-04-13 20:31:06 +0200

    add pack

--- src/_data/packstack.mjs
+++ src/_data/packstack.mjs
@@ -181,6 +181,7 @@ export default async () => {
 	packstack.packs.push(await getPack('909'));
 	packstack.packs.push(await getPack('1054'));
 	packstack.packs.push(await getPack('1347'));
+	packstack.packs.push(await getPack('2123'));
 
 	packstack.packs.sort()
 
